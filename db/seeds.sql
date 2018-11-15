af9c2b44da5ac2d26995a31cee6a445058a276ca
Schlueter <SchlueterPro@gmail.com>
2018-11-15 11:29:14 -0800

    commiting changes

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (name, sleepy) VALUES ('Spicy Cheeseburger', true);
+INSERT INTO burgers (name, sleepy) VALUES ('Cheeseless Cheeseburger', true);
+INSERT INTO burgers (name, sleepy) VALUES ('Extra Cheezy Cheeseburger', true);
+INSERT INTO burgers (name) VALUES ('5 Pouund Cheeseburger');
