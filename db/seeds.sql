227965992626b82f61be0889d983dc4eafc710bb
Schlueter <SchlueterPro@gmail.com>
2018-11-17 12:53:11 -0800

    almost there

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
 INSERT INTO burgers (name) VALUES ('Cheeseburger');
 INSERT INTO burgers (name) VALUES ('Bacon Cheeseburger');
-INSERT INTO burgers (name, sleepy) VALUES ('Spicy Cheeseburger', true);
-INSERT INTO burgers (name, sleepy) VALUES ('Cheeseless Cheeseburger', true);
-INSERT INTO burgers (name, sleepy) VALUES ('Extra Cheezy Cheeseburger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Spicy Cheeseburger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Cheeseless Cheeseburger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Extra Cheezy Cheeseburger', true);
 INSERT INTO burgers (name) VALUES ('5 Pouund Cheeseburger');
